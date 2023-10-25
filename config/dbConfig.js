4aee97b580a033f917f79e379cbefca7bcb63a98
karanhanda07 <147022308+karanhanda07@users.noreply.github.com>
2023-10-24 23:31:45 -0400

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://karanhanda40:KaranHanda@marketplace.hxa4bzw.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://karanhanda40:...........@marketplace.hxa4bzw.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
